ef755940cdfddbe9b2e49d38ac3f8f069c1d29bd
Walaeddine <walaeddine.web.dev@gmail.com>
2022-07-25 03:46:33 +0100

    V0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const compression=require('compression')
 app=express()
 app.use(compression())
 app.use( express.static(__dirname+'/dist/chocolate-shop'))
-app.get('/*',(req,res)=>{res.sendFile('F:/run app/dist/walaeddine/index.html')})
+app.get('/*',(req,res)=>{res.sendFile('index.html',{root:'./dist/chocolate-shop/'})})
 
 const port=3000;
 app.listen(port,()=>{console.log('running on port '+port)})
