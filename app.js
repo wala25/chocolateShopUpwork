1c647bda8f03cf3439786dc29b13aba908dc5555
Walaeddine <walaeddine.web.dev@gmail.com>
2022-07-28 14:12:26 +0100

    deploy

--- app.js
+++ app.js
@@ -11,5 +11,6 @@ app.post('/api/postform', sendmail.sendMail)
 app.use( express.static(__dirname+'/dist/chocolate-shop'))
 app.get('/*',(req,res)=>{res.sendFile('index.html',{root:'./dist/chocolate-shop/'})})
 
-const port=3000;
+const port=process.env.PORT || 3000;
+
 app.listen(port,()=>{console.log('running on port '+port)})
