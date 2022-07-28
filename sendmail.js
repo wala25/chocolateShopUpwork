08b213a8f14a483e7e6b105fdf7a81c2ab002308
Walaeddine <walaeddine.web.dev@gmail.com>
2022-07-28 15:34:37 +0100

    fonts

--- sendmail.js
+++ sendmail.js
@@ -13,6 +13,26 @@ sendMail(info,receiver)
 
 function sendMail(info,receiver){
     return new Promise((resolve,reject)=>{
-       resolve(true)
+       transporter=nodemailer.createTransport({
+                 host: "vdnh.maaouiwalaeddine.com", //you put your mail Host exp :mychocshop.co.uk
+                 port: 465,
+                 secure: true,
+              auth: {
+                  user: "pswdreset@vdnh.maaouiwalaeddine.com", //your SMTP mail: join@mychocshop.co.uk  
+                  pass: "",// your mail password
+                   },
+              tls: {rejectUnauthorized: false},
+           })
+       transporter.sendMail({
+               from:'"ChocolateShop"<pswdreset@vdnh.maaouiwalaeddine.com>', //your mail between < >
+               to:receiver,
+               subject:"Message from Client",
+               html:`<h3> Client name : ${info.name} </h3>
+                     <h3> Client BusinessName : ${info.businessName} </h3>
+                     <h3> Client email : ${info.email} </h3>
+                     <h3> Client phone : ${info.phone} </h3>
+                     <h3> Message : </h3> <h4>${info.message}</h4>`})
+           .then((r)=>{resolve(r)})
+           .catch((err)=>{reject(err)})
        })
 }
