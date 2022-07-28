0e9677124a1cdd054d0f3bf08b26d90e06be7cf4
Walaeddine <walaeddine.web.dev@gmail.com>
2022-07-28 02:36:51 +0100

    progress1

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express=require('express')
 const compression=require('compression')
 app=express()
 app.use(compression())
+app.use(express.json())
 app.use( express.static(__dirname+'/dist/chocolate-shop'))
+app.post('/api/postform', (req,res)=>{
+    console.log(req.body)
+})
 app.get('/*',(req,res)=>{res.sendFile('index.html',{root:'./dist/chocolate-shop/'})})
 
 const port=3000;
